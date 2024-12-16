06fc1f967c27538c962b076575efeab0f357e7fe
Francesco Celentano <francesco.celentano19@gmail.com>
2024-12-16 19:07:59 +0100

    Second Commit

--- Scontrino.py
+++ Scontrino.py
@@ -1,1 +1,9 @@
-#prova
+#prova di acquisto e calcolo di spesa
+
+Cosa = input("cosa vuoi comprare? ")
+prezzo = input("quanto costa? ")
+qnt = input("quante ne vuoi comprare? ")
+
+tot = float(prezzo) + int(qnt)
+
+print("hai appena speso " + str(tot) + "$")
