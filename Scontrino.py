1f59f1c533e2edb40f634e9e36316c8c082b725a
Francesco Celentano <francesco.celentano19@gmail.com>
2024-12-17 09:46:27 +0100

    metodi scontrino Commit

--- Scontrino.py
+++ Scontrino.py
@@ -1,9 +1,20 @@
 #prova di acquisto e calcolo di spesa
-
-Cosa = input("cosa vuoi comprare? ")
-prezzo = input("quanto costa? ")
+#metodo 1
+item = input("cosa vuoi comprare? ")
+price = input("quanto costa? ")
 qnt = input("quante ne vuoi comprare? ")
 
-tot = float(prezzo) + int(qnt)
+tot = round(float(price) * int(qnt), 2)
 
-print("hai appena speso " + str(tot) + "$")
+print("hai appena speso " + str(tot) )
+
+#metodo 2
+print(f"Per comprare {qnt} di {item} devi pagare {round(int(qnt) * float(price), 2)}")
+
+#metodo 3
+item1 = input("cosa vuoi comprare? ")
+price1 = float(input("quanto costa in $? "))
+qnt1 = int(input("quante ne vuoi comprare? "))
+
+
+print(f"per comprare {qnt1} di {item1} devi pagare {round(qnt1 * price1, 2)}$")
