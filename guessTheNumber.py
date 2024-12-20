523fbf93d28894a8a7327f04c2c774e08808a116
Francesco Celentano <francesco.celentano19@gmail.com>
2024-12-20 10:52:04 +0100

    guessTheNumber+cicloWhile applicato commit

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -4,20 +4,34 @@ numero_segreto = random.randint(1,100)
 print(numero_segreto)
 print("\nBenvenuto alla ruota della fortuna")
 
-tentativi = 7
+tentativi = 5
 
 while tentativi > 0:
-    num = int(input("Prova ad indovinare il numero").strip())
+    print(f"Hai {tentativi} tentativi ")
+    num = ""
+    while not num.isdigit():
+        num = input("Prova ad indovinare il numero (solo numeri consentiti) ").strip()
+    else:
+        num = int(num)
+
     tentativi -= 1
     if num == numero_segreto:
         print("Hai indovinato il numero!!! ")
         break
-    elif num < numero_segreto:
-        print("Il numero che cerchi è maggiore")
-    elif num > numero_segreto:
-        print("Il numero che cerchi è minore")
-else:
-    print(f"Tentativi finiti " f"il numero era {numero_segreto}, ritenta. ")
+    else:
+        print("Hai sbagliato! ")
+        if tentativi == 0:
+            print(f"Il numero era {numero_segreto}, ritenta. ")
+            continue
+        elif num < numero_segreto and tentativi != 0:
+            print("Il numero che cerchi è maggiore ")
+        elif num > numero_segreto and tentativi != 0:
+            print("Il numero che cerchi è minore ")
+        #elif tentativi <= 5:
+            #print(f"ti rimangono {tentativi} tentativi")
+
+#else:
+   # print(f"Non hai piu tentativi \nIl numero era {numero_segreto}, ritenta. ")
 
 
 
